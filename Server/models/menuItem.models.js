1151e297649a3035a7bf12dc63d62e063a3c67aa
xavierkooijman <xavierkooijman@gmail.com>
2025-09-24 13:12:18 +0200

    check if menu item already exists in the restaurant model function

--- Server/models/menuItem.models.js
+++ Server/models/menuItem.models.js
@@ -2,9 +2,22 @@ const db = require("../config/db");
 
 const createMenuItem = async (data) => {
   const { restaurantId, name, price } = data;
-  const query = "INSERT INTO menu_items VALUES($1, $2, $3)";
+  const query =
+    "INSERT INTO menu_items(restaurant_id, item_name, price) VALUES($1, $2, $3)";
 
   values = [restaurantId, name, price];
   const { rows } = await db.writePool.query(query, values);
+  return rows[0];
+};
+
+const checkIfMenuItemExists = async (data) => {
+  const { restaurantId, name } = data;
+
+  const query =
+    "SELECT id FROM menu_items WHERE restaurant_id = $1 AND name = $2";
+
+  values = [restaurantId, name];
+
+  const { rows } = await db.readPool.query(query, values);
   return rows[0];
 };
