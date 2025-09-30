d7ee8097bbb8c42177c83cb3bab3f68678413005
xavierkooijman <xavierkooijman@gmail.com>
2025-10-01 00:06:47 +0200

    route for selecting a current restaurant for the tenant, updating the tokens with new tenant context

--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -4,6 +4,7 @@ const authController = require("../controllers/auth.controllers");
 const validate = require("../middleware/validator");
 const userValidation = require("../validation/user.validation");
 const refreshTokenController = require("../controllers/refreshToken.controllers");
+const verifyToken = require("../middleware/verifyJWT");
 
 router.post(
   "/login",
@@ -12,5 +13,10 @@ router.post(
 );
 router.get("/refresh", refreshTokenController.handleRefreshToken);
 router.post("/logout", authController.logoutHandler);
+router.post(
+  "/select-restaurant",
+  verifyToken,
+  authController.enterRestaurantDomain
+);
 
 module.exports = router;
