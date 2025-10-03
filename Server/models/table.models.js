c683f4d334fca0c549ed7d8b15ffedd016327c81
xavierkooijman <xavierkooijman@gmail.com>
2025-10-03 19:41:37 +0100

    table model, get tables by location, by status, get all tables

--- Server/models/table.models.js
+++ Server/models/table.models.js
@@ -47,7 +47,68 @@ const checkIfTableExists = async (restaurantId, tableNumber) => {
   return rows[0];
 };
 
+const getAllTables = async (restaurantId) => {
+  const query = `
+    SELECT t.id, t.table_number, t.capacity, t.position_x, t.position_y, t.rotation,
+            ts.shape_name, tl.table_location, tstatus.status
+    FROM tables t
+    JOIN table_shapes ts ON t.shape_id = ts.id
+    JOIN table_locations tl ON t.location_id = tl.id
+    JOIN table_status tstatus ON t.status_id = tstatus.id
+    WHERE t.restaurant_id = $1
+    ORDER BY t.table_number
+  `;
+
+  await db.readPool.query("BEGIN");
+  await db.readPool.query(`SET LOCAL app.jwt_restaurantId = ${restaurantId}`);
+  const { rows } = await db.readPool.query(query, [restaurantId]);
+  await db.readPool.query("COMMIT");
+  return rows;
+};
+
+const getTablesByLocation = async (restaurantId, locationId) => {
+  const query = `
+    SELECT t.id, t.table_number, t.capacity, t.position_x, t.position_y, t.rotation,
+            ts.shape_name, tl.table_location, tstatus.status
+    FROM tables t
+    JOIN table_shapes ts ON t.shape_id = ts.id
+    JOIN table_locations tl ON t.location_id = tl.id
+    JOIN table_status tstatus ON t.status_id = tstatus.id
+    WHERE t.restaurant_id = $1 AND t.location_id = $2
+    ORDER BY t.table_number
+  `;
+  const values = [restaurantId, locationId];
+
+  await db.readPool.query("BEGIN");
+  await db.readPool.query(`SET LOCAL app.jwt_restaurantId = ${restaurantId}`);
+  const { rows } = await db.readPool.query(query, values);
+  await db.readPool.query("COMMIT");
+  return rows;
+};
+
+const getTablesByStatus = async (restaurantId, statusId) => {
+  const query = `
+    SELECT t.id, t.table_number, t.capacity, t.position_x, t.position_y, t.rotation,
+            ts.shape_name, tl.table_location, tstatus.status
+    FROM tables t
+    JOIN table_shapes ts ON t.shape_id = ts.id
+    JOIN table_locations tl ON t.location_id = tl.id
+    JOIN table_status tstatus ON t.status_id = tstatus.id
+    WHERE t.restaurant_id = $1 AND t.status_id = $2
+    ORDER BY t.table_number
+  `;
+  const values = [restaurantId, statusId];
+  await db.readPool.query("BEGIN");
+  await db.readPool.query(`SET LOCAL app.jwt_restaurantId = ${restaurantId}`);
+  const { rows } = await db.readPool.query(query, values);
+  await db.readPool.query("COMMIT");
+  return rows;
+};
+
 module.exports = {
   createTable,
   checkIfTableExists,
+  getAllTables,
+  getTablesByLocation,
+  getTablesByStatus,
 };
